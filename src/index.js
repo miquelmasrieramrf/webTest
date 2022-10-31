32e15435b003070f81b6832b62e0675c2c15b64f
Miquel Masriera <miquelmasriera@Miquels-MacBook-Pro.local>
2022-10-31 01:17:31 +0100

    finised

--- src/index.js
+++ src/index.js
@@ -26,15 +26,16 @@
 		articles.forEach(article => {
 			const li = document.createElement('li');
 
-			li.classList.add('article');
-
+			li.classList.add('article', 'tile');
 			li.innerHTML = `
 				<a href="" style="text-decoration: none; color: #FFF;">
-					<p style="font-size: 1.5rem; font-weight: bold; margin: 1rem 0 0;">
+					<p style="font-size: 1.25rem; font-weight: bold; margin: 1rem 0 0.5rem;">
 						${ article.title }
 					</p>
-					<p>${ article.subtitle }</p>
-					<p style="text-align: right; margin-bottom: 0;">${ article['author'] }</p>
+					<p style="margin: 0.5rem 0; border-top: 1px solid #FFF;">${ article.subtitle }</p>
+					<p style="text-align: right; margin-bottom: 0; font-style: italic; font-size: 0.8rem;">
+						${ article['author'] }
+					</p>
 				</a>
 			`;
 
